122ad78e33fc404254ee0288a0d5004b70dc3a88
Arkanic <50847107+Arkanic@users.noreply.github.com>
2019-08-23 08:27:44 +1200

    finished

--- names.js
+++ names.js
@@ -4,12 +4,24 @@ wc.suffixes = ["", ""];
 wc.prefix = "";
 wc.suffix = "";
 
-wc.changePrefix = function() {
-  wc.prefix = prefixes[Math.floor(Math.random() * wc.prefixes.length)];
+wc.display = function() {
+  document.getElementById("name-text").innerHTML = wc.prefix + wc.suffix;
 }
 
-
-wc.init = function() {
+wc.changePrefix = function() {
+  wc.prefix = prefixes[Math.floor(Math.random() * wc.prefixes.length)];
+  wc.display();
+}
+wc.changeSuffix = function() {
+  wc.suffix = suffixes[Math.floor(Math.random() * wc.suffixes.length)];
+  wc.display();
+}
+wc.changeAll = function() {
   wc.changePrefix();
   wc.changeSuffix();
+  wc.display();
+}
+
+wc.init = function() {
+  wc.changeAll();
 }
