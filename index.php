58ae06c85653b567830317a729a8fa9c04e74ef8
Arkanic <50847107+Arkanic@users.noreply.github.com>
2019-08-23 08:35:20 +1200

    removed wc

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
     <script src="names.js"></script>
     <link href="css/global.css" type="text/css" rel="stylesheet"/>
   </head>
-  <body onLoad="wc.init()">
+  <body onLoad="init()">
     <p id="name-text">~</p>
     <p>
-      <button onclick="wc.changePrefix()">Change Prefix</button>
-      <button onclick="wc.changeAll()">Change Name</button>
-      <button onclick="wc.changeSuffix()">Change Suffix</button>
+      <button onclick="changePrefix()">Change Prefix</button>
+      <button onclick="changeAll()">Change Name</button>
+      <button onclick="changeSuffix()">Change Suffix</button>
     </p>
   </body>
 </html>
